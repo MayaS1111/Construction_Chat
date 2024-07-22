4bf8090de2165f35d2151664268bbb8560044fa0
MayaS111 <mayasheriff2018@gmail.com>
2024-07-22 14:37:35 -0500

    added devise and scaffolded projects with references

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :projects
+  root chats#index
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
