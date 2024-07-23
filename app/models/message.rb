435a076139c915ace9725e7700d713bbabc19455
MayaS111 <mayasheriff2018@gmail.com>
2024-07-23 15:52:57 -0500

    migrated sender id (correctly)

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,10 +6,16 @@
 #  body         :text
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
+#  sender_id    :integer          not null
 #  user_chat_id :integer
+#
+# Indexes
+#
+#  index_messages_on_sender_id  (sender_id)
 #
 # Foreign Keys
 #
+#  sender_id     (sender_id => users.id)
 #  user_chat_id  (user_chat_id => user_chats.id)
 #
 class Message < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_164418) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_204806) do
   create_table "chats", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_164418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_chat_id"
+    t.integer "sender_id", null: false
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
   end
 
   create_table "projects", force: :cascade do |t|
@@ -69,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_164418) do
 
   add_foreign_key "chats", "projects"
   add_foreign_key "messages", "user_chats"
+  add_foreign_key "messages", "users", column: "sender_id"
   add_foreign_key "projects", "users", column: "owner_id"
   add_foreign_key "user_chats", "chats"
   add_foreign_key "user_chats", "users"
