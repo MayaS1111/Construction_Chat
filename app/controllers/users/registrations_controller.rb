76c2ff4390d0f9a610ec28f99fc5eb9f67b25d90
MayaS111 <mayasheriff2018@gmail.com>
2024-08-03 18:39:32 -0500

    connected registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   def create
     Rails.logger.info("Custom user creation logic")
+    
     super
+
     project = Project.create(owner_id: current_user.id, name: "#{current_user.first_name}'s DMs", description: "nil", location: "nil", project_type: "private", status: "nil")
     chat = Chat.create(project_id: project.id, name: "New DM", description: "This chat is for all members")
     UserChat.create(user_id: current_user.id, chat_id: chat.id)
+
     Rails.logger.info("Custom user creation completed")
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
  
   root to: "users#home"
 
-  devise_for :users#, controllers: {
-    # registrations: 'users/registrations'
-  # }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+   }
 
   resources :messages
   resources :user_chats
