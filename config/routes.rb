b29bf146f53ca5be76e013220e85053ca413c3ff
MayaS111 <mayasheriff2018@gmail.com>
2024-08-03 16:24:46 -0500

    sign up page has all needed inputs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
  
   root to: "users#home"
 
-  devise_for :users 
+  devise_for :users#, controllers: {
+    # registrations: 'users/registrations'
+  # }
 
   resources :messages
   resources :user_chats
