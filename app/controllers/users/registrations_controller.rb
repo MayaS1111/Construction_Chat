42ef4260a65d2080066071adef38bbb9c8269b08
MayaS111 <mayasheriff2018@gmail.com>
2024-08-03 19:16:07 -0500

    all signup fields now get saved to database

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,22 +2,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     Rails.logger.info("Custom user creation logic")
 
-    super
-
-    if resource.persisted?  # Check if user was successfully created
-      begin
-        # Create a new Project and Chat
-        project = Project.create!(owner_id: resource.id, name: "#{resource.first_name}'s DMs", description: "nil", location: "nil", project_type: "private", status: "nil")
-        chat = Chat.create!(project_id: project.id, name: "New DM", description: "This chat is for all members")
-        UserChat.create!(user_id: resource.id, chat_id: chat.id)
-
-        Rails.logger.info("Custom user creation completed successfully")
-      rescue StandardError => e
-        Rails.logger.error("Error during custom user creation: #{e.message}")
-        # Handle errors or rollback transactions if needed
+    super do |resource|
+      # Custom logic after user creation
+      Rails.logger.info("Custom user creation logic")
+      
+      if resource.persisted?
+        begin
+          project = Project.create!(owner_id: resource.id, name: "#{resource.first_name}'s DMs", description: "nil", location: "nil", project_type: "private", status: "nil")
+          chat = Chat.create!(project_id: project.id, name: "New DM", description: "This chat is for all members")
+          UserChat.create!(user_id: resource.id, chat_id: chat.id)
+        rescue StandardError => e
+          Rails.logger.error("Error during custom user creation: #{e.message}")
+          # Handle errors or rollback transactions if needed
+        end
       end
-    else
-      Rails.logger.error("User was not successfully created")
     end
   end
 end
