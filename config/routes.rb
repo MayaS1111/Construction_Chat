4bf964c43551e2c8701c615b87bfb4f99299bf7d
MayaS111 <mayasheriff2018@gmail.com>
2024-08-05 16:19:01 -0500

    added admin auth in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  authenticate :user, ->(user) { user.admin? } do
+    mount RailsAdmin::Engine, at: "admin", as: "rails_admin"
+  end
    
   resources :messages
   resources :user_chats
