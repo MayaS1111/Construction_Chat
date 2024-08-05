c285f38079b7088d780c879c1c6c9db618d581f9
MayaS111 <mayasheriff2018@gmail.com>
2024-08-05 16:17:21 -0500

    added admin to users with default false

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
-#  admin                  :boolean
+#  admin                  :boolean          default(FALSE), not null
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
