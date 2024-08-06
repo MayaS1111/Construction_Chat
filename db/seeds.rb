fbe4963dadb269da924f4911ca59ebabf69b8674
MayaS111 <mayasheriff2018@gmail.com>
2024-08-06 11:45:53 -0500

    seed works with out if

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 if User.where.not(id: "0").exists?
-  admin_bot = User.create(id: 0, first_name: "BuiltBetter", last_name: "Bot", phone_number: "(000)000-0000", email: "builtbetterbot@example.com", job_title: "Helper Bot", password: "password", admin: "true", profile_image: "https://api.dicebear.com/9.x/thumbs/svg?seed=Sam&radius=50&scale=70&shapeColor=000000&backgroundColor=D2042D")
+  admin_bot = User.create!(id: 0, first_name: "BuiltBetter", last_name: "Bot", phone_number: "(000)000-0000", email: "builtbetterbot@example.com", job_title: "Helper Bot", password: "password", admin: "true", profile_image: "https://api.dicebear.com/9.x/thumbs/svg?seed=Sam&radius=50&scale=70&shapeColor=000000&backgroundColor=D2042D")
   admin_bot.save!
+  puts 'Default admin user created'
+else
+  puts 'Admin user already exists'
 end
