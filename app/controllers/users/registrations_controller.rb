14ec4e282284b6499507aa80e54f1bf3ba96ecf2
MayaS111 <mayasheriff2018@gmail.com>
2024-08-06 19:49:03 -0500

    edit to registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,15 +10,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
         begin
           project = Project.create!(owner_id: resource.id, name: "#{resource.first_name}'s DMs", description: "nil", location: "nil", project_type: "private", status: "nil")
 
-          unless User.where(id: "0").exists?
+          unless User.where(id: 0).exists?
             chat_bot = User.create(id: 0, first_name: "BuiltBetter", last_name: "Bot", phone_number: "0000000000", email: "buildbetter@info.com", job_title: "Helper Bot", password: "password", admin: "true", profile_image: "https://api.dicebear.com/9.x/thumbs/svg?seed=Sam&radius=50&scale=70&shapeColor=000000&backgroundColor=D2042D")
           else
-            chat_bot = User.find_by(id: "0")
+            chat_bot = User.find_by(id: 0)
           end
 
           chat = Chat.create!(project_id: project.id, name: "#{resource.first_name} & #{chat_bot.name}", description: "nil")
           UserChat.create!(user_id: resource.id, chat_id: chat.id)
-          UserChat.create!(user_id: chat_bott.id, chat_id: chat.id)
+          UserChat.create!(user_id: chat_bot.id, chat_id: chat.id)
           Message.create(body: "Welcome to BuiltBetter! Please message here for any help you my need with the application. Even application feedback is welcomed!", sender_id: chat_bot.id, chat_id: chat.id)
 
         rescue StandardError => e
