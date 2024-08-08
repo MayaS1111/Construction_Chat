cca05385e212814d66695ecfb2d5d240d3ad37b4
MayaS111 <mayasheriff2018@gmail.com>
2024-08-07 19:55:59 -0500

    user profile, members list, and removed button margin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ profile all_users members ]
   
-
-
+  
   private
 
     def set_user
       @users = User.all
       if params[:id]
         @user = User.find_by!(id: params.fetch(:id))
+        
       else
         @user = current_user
+        if params[:user]
+          @user_profile = User.find_by!(id: params.fetch(:user))
+        end
       end
     end
 end
