4f17114993c84af08d727cb71b5461af2ef12147
MayaS111 <mayasheriff2018@gmail.com>
2024-08-15 10:00:55 -0500

    default profile image when creating an account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :profile_image])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :profile_image, :job_title])
     end
 end
